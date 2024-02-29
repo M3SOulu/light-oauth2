51a3a88681f1ec84cea6115d7ec89cfa39701b32
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-02-29 16:08:03 +0200

    add: PKCE task and code challenge

--- locust/locustfiles/locust_token.py
+++ locust/locustfiles/locust_token.py
@@ -3,6 +3,9 @@ from locust.exception import RescheduleTask
 
 import logging
 from urllib.parse import urlparse, parse_qs
+from uuid import uuid4
+from hashlib import sha256
+from base64 import b64encode
 
 from .locust_client import CLIENTS
 
@@ -89,6 +92,62 @@ class OAuthUser(HttpUser):
             else:
                 r = r.json()
                 logging.warning(f"Access Token Authorization Code Flow: Did not get code 200, code is {r['statusCode']}, "
+                             f"error code is {r['code']}")
+            self.user.auth_code = None
+            self.interrupt()
+
+        def on_stop(self):
+            CLIENTS.add(self.user.cl)
+
+    @task(1)
+    class AuthorizationCodeFlowPKCE(SequentialTaskSet):
+
+        def on_start(self):
+            try:
+                self.user.cl = CLIENTS.pop()
+            except KeyError:
+                self.interrupt()
+            self.code_verifier = str(uuid4()) + str(uuid4)
+            self.code_challenage = b64encode(sha256(self.code_verifier.encode('utf-8')).digest())
+
+        @task(1)
+        def access_code_pkce(self):
+            r = self.client.get(
+                f"{self.user.code_host}/oauth2/code", params={
+                    "response_type": "code",
+                    "client_id": self.user.cl.clientId,
+                    "redirect_uri": "http://localhost:8080/authorization"
+                },
+                auth=('admin', '123456'),
+                verify=False,
+                allow_redirects=False)
+            if r.status_code == 302:
+                parsed_redirect = urlparse(r.headers['Location'])
+                redirect_params = parse_qs(parsed_redirect.query)
+                self.user.auth_code = redirect_params.get('code')[0]
+                logging.info(f"Auth Code: ClientId = {self.user.cl.clientId}, Authorization_code = {self.user.auth_code}")
+            else:
+                logging.info("Auth Code: Endpoint did not redirect")
+
+        @task
+        def access_token_authorization_code_flow_pkce(self):
+            r = self.client.post(
+                f"{self.user.token_host}/oauth2/token", data={
+                    "grant_type": "client_credentials",
+                    "code": self.user.auth_code,
+                    "redirect_uri": "http://localhost:8080/authorization"
+                },
+                auth=(self.user.cl.clientId, self.user.cl.clientSecret),
+                verify=False,
+                allow_redirects=False)
+            if r.status_code == 200:
+                r = r.json()
+                self.user.access_token = r['access_token']
+                logging.info(
+                    f"Access Token Authorization Code Flow: ClientId = {self.user.cl.clientId}, Access Token = {self.user.access_token}")
+            else:
+                r = r.json()
+                logging.info(f"Access Token Authorization Code Flow: Did not get code 200, code is {r['statusCode']}, "
                              f"error code is {r['code']}")
             self.user.auth_code = None
             self.interrupt()
