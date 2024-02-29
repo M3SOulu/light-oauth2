0e1f1964c49cd064bd9ecbf4919a2e78cc6a92f4
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-02-29 10:45:42 +0200

    add: User dataclass

--- locust/locustfiles/locust_user.py
+++ locust/locustfiles/locust_user.py
@@ -14,7 +14,21 @@ USERS = set()
 
 @dataclass
 class User:
-    pass
+    userId: str = field(default_factory=lambda: str(uuid4())[:8], repr=True, hash=True)
+    firstName: str = field(default_factory=lambda: str(uuid4())[:32], repr=False, hash=False)
+    lastName: str = field(default_factory=lambda: str(uuid4())[:32], repr=False, hash=False)
+    userType: str = field(default="admin", repr=True, hash=False)
+    email: str = field(default='first.last@oulu.fi', repr=False, hash=False)
+    password: str = field(default_factory=lambda: str(uuid4()), repr=False, hash=False)
+
+    def to_dict(self):
+        return {'userId': self.userId,
+                'userType': self.userType,
+                'firstName': self.firstName,
+                'lastName': self.lastName,
+                'email': self.email,
+                'password': self.password,
+                'passwordConfirm': self.password}
 
 
 class UserRegistration(HttpUser):
